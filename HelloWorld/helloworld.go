31262389774115bf88a306ad85daf886dd9757b6
Micha Rösler <micha.roesler@tivian.com>
2022-04-04 00:01:20 +0200

    update helloworld with short declaration operator

--- HelloWorld/helloworld.go
+++ HelloWorld/helloworld.go
@@ -5,15 +5,22 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World")
 	foo()
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 10; i++ {
 		if i%2 == 0 {
 			fmt.Println(i)
 		}
 	}
+	bar()
 }
 
 func foo() {
 	fmt.Println("I am in the FOO function")
+}
+
+func bar() {
+	var myAge int = 41
+	myAge++
+	fmt.Printf("%s%d%s", "Next year i will be ", myAge, " years old.\n")
 }
 
 // control flow:
