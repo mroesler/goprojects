92fb5642090cd673b7cfc62eda16092d605d0139
Micha Rösler <micha.roesler@tivian.com>
2022-04-05 22:32:58 +0200

    chapter1 exercises

--- Hello/hello.go
+++ Hello/hello.go
@@ -6,11 +6,42 @@ import (
 	"rsc.io/quote"
 )
 
+type MICHA int
+
+var x MICHA
+
+//var x int = 42
+var y string = "James Bond"
+var z bool = true
+
 func main() {
-	fmt.Println(hello())
+	//hello()
+	//assignVariables()
+	// fmt.Println(x, y, z)
+	//concatXYZ()
+
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Println(x)
 }
 
-func hello() string {
+func hello() {
+	fmt.Println(quote.Hello())
+}
 
-	return quote.Hello()
+func assignVariables() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Printf("%d\n%s\n%t\n", x, y, z)
+	fmt.Printf("%T\n", x)
+	fmt.Printf("%T\n", y)
+	fmt.Printf("%T\n", z)
+}
+
+func concatXYZ() {
+	s := fmt.Sprintf("%d %s %t", x, y, z)
+	fmt.Println(s)
 }
